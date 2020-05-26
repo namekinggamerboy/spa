1f742075b5b62a6834766fbf297c7acc421ed6a5
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-05-26 13:13:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const AntiSpam = require("discord-anti-spam");
 const Discord = require("discord.js");
-const antiSpam = new AntiSpam({
-  warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
-  kickThreshold: 7, // Amount of messages sent in a row that will cause a ban.
-  banThreshold: 10, // Amount of messages sent in a row that will cause a ban.
-  maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
-  warnMessage: "{@user}, Please stop spamming.", // Message that will be sent in chat upon warning a user.
-  kickMessage: "**{user_tag}** has been kicked for spamming.", // Message that will be sent in chat upon kicking a user.
-  banMessage: "**{user_tag}** has been banned for spamming.", // Message that will be sent in chat upon banning a user.
-  maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
-  maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
-  maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
-  exemptPermissions: ["ADMINISTRATOR", "MANAGE_MESSAGES"], // Bypass users with any of these permissions.
-  ignoreBots: false, // Ignore bot messages.
-  verbose: true, // Extended Logs from module.
-  ignoredUsers: [] // Array of User IDs that get ignored.
-  // And many more options... See the documentation.
-});
 
 module.exports = {
 
   version: require("./package.json").version,
 
   checkspam(client){
-  if (!client)
+  if (!client.bot)
     return console.log(
       "[anti-raid]{type: error} ⚠️: make sure check your code(made by. Name boy and Οㄗ│Captaiℵ)"
     );
 
   client.on("ready", () => console.log(`[anti-raid]☑️: successfully active anti-raid(made by. Name boy and Οㄗ│Captaiℵ)`));
   
+const antiSpam = new AntiSpam({
+  warnThreshold: client.warn||3,
+  kickThreshold: client.kick||7,
+  banThreshold: client.ban||7,
+  maxInterval: client.interval||2000,
+  warnMessage: "{@user}, Please stop spamming.",
+  kickMessage: "**{user_tag}** has been kicked for spamming.",
+  banMessage: "**{user_tag}** has been banned for spamming.",
+  maxDuplicatesWarning: client.maxwarn||7,
+  maxDuplicatesKick: client.maxkick||10,
+  maxDuplicatesBan: client.maxban||12,
+  exemptPermissions: client.permission||["ADMINISTRATOR", "MANAGE_MESSAGES"], 
+  ignoreBots: client.bot||false,
+  verbose: client.verbose||true,
+  ignoredUsers: client.ignoreduser||[]
+});
+    
   client.on("message", message => antiSpam.message(message));
 
   // This is for all links
@@ -73,19 +73,16 @@ module.exports = {
       `discord gg /`
     ];
     try {
+      let version = parseInt(Discord.version.split(".")[0], 10) < 12 ? "v11" : "v12";
       if (
         bannedWords.some(word => message.content.toLowerCase().includes(word))
       ) {
-        if (message.author.id === "") return; // This is to ignore a user maybe you
-        if (message.channel.id === "") return; //ignore channel
-        if (message.channel.id === "") return; //ignore channel
         await message.delete();
         await message.channel
           .send(`${message.author}`, {embed:{ title: `Dont send invite links here!`, color: 0xff0000}})
           .then(sentMessage => {
-let version = parseInt(Discord.version.split(".")[0], 10) < 12 ? "v11" : "v12";
            if(version === "v11"){
-            sentMessags.delete(5000);
+            sentMessage.delete(5000);
            } else {
             sentMessage.delete({ timeout: 5000, reason: "send link" });
            }
